23394dc3dd416950cbbc4f7f0186846b16882d06
Grassi <grassiricardorg@gmail.com>
2017-06-10 00:22:08 -0300

    passing properts of components son

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
   <div>
-    <SilvaFamily/>
+    <Family lastName="Silva">
+      <Member name="Guilherme"/>
+      <Member name="Rafael"/>
+      <Member name="juliano"/>
+    </Family>
   </div>
 , document.getElementById('app'))
 
