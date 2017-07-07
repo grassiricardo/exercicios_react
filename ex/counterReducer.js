a0b2e6f2ce8da42d96f4672c7274a8015963a0ff
Grassi <grassiricardorg@gmail.com>
2017-07-06 22:39:00 -0300

    finish counter component

--- ex/counterReducer.js
+++ ex/counterReducer.js
@@ -1,4 +1,4 @@
-const INITIAL_STATE = { step: 1, number: 0}
+const INITIAL_STATE = { step: 1, number: 0 }
 
 export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
@@ -7,7 +7,7 @@ export default function(state = INITIAL_STATE, action) {
         case 'DEC':
             return  { ...state, number: state.number - state.step }
         case 'STEP_CHANGED':
-            return  { ...state, step: action.payload }
+            return  { ...state, step: +action.payload }
         default:
             return state
     }
